917792771c43ed9e21be020f7495a5038c4d98e5
kimteespk <supakorn.saei@bumail.net>
2023-01-10 22:50:26 +0700

    dashboard design pic

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 import new_main # ! DONT FORGET TO RENAME
 from dash import Dash, html, dcc
 
-# Copy dash code here
+# Copy dash code paste here
+"""
+
+MAIN APPICATION, start dash web app by excecute this module
+ALL GRAPH FUNCTION WILL CODE BELOW
+Ralative functions are imported from new_main.py,
+
+When GET COMMENT FROM YOUTUBE are clicked, dash will call FetchYouTubeData function
+THEN DB class
+
+If user select listbox of existing VDO to plot then call DataProcessing class and their functions 
+    which return a df ready for plot (maybe acess df by class property directly if it better)
+    
+"""
 
 
 
